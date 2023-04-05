9211dbcfaf272fcb8eddd65dbaaf484013b30fe1
atul <atul.vaibhav1993@gmail.com>
2023-04-05 09:56:13 +0530

    Commit -5/4/2023

---
+++ Fundamentals/src/com/javalearning/OOP/ExceptionHandling/Try_Finally.java
@@ -0,0 +1,22 @@
+package com.javalearning.OOP.ExceptionHandling;
+
+public class Try_Finally {
+    
+	public static void m1(){
+		try{
+			//System.out.println("Inside try-block");
+			System.out.println(12/0);
+		}finally{
+			System.out.println("Inside finally block");
+			System.out.println();
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Try_Finally.m1();
+
+	}
+
+}
